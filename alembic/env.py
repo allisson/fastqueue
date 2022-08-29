fe1fc4456398c2d1c6d3435d6aebc3a0887da386
Allisson Azevedo <allisson@gmail.com>
2022-08-29 19:38:37 -0300

    feat: use only sqlalchemy for orm (#5)

--- alembic/env.py
+++ alembic/env.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 
 from alembic import context
 from fastqueue.config import settings
-from fastqueue.models import metadata
+from fastqueue.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -19,7 +19,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = metadata
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- alembic/versions/001_initial.py
+++ alembic/versions/001_initial.py
@@ -1,8 +1,8 @@
 """Auto generated
 
-Revision ID: 1f5fd70b6d4d
+Revision ID: 4485cf5c36b9
 Revises:
-Create Date: 2022-08-29 15:17:54.064031
+Create Date: 2022-08-29 18:44:21.413531
 
 """
 import sqlalchemy as sa
@@ -11,7 +11,7 @@ from sqlalchemy.dialects import postgresql
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = "1f5fd70b6d4d"
+revision = "4485cf5c36b9"
 down_revision = None
 branch_labels = None
 depends_on = None
