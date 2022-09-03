08ccad7b3adc8a4e506177ea83ab775dc38aec48
Allisson Azevedo <allisson@gmail.com>
2022-09-03 19:48:58 -0300

    feat: add TopicService (#7)
    * feat: add TopicService
    
    * fix: fix session type hint
    
    * test: add more tests
    
    * test: fix cache path

---
+++ tests/test_services.py
@@ -0,0 +1,48 @@
+import pytest
+
+from fastqueue.exceptions import NotFoundError
+from fastqueue.schemas import CreateTopicSchema
+from fastqueue.services import TopicService
+
+
+def test_topic_service_create(session):
+    id = "my_topic"
+    data = CreateTopicSchema(id=id)
+    service = TopicService()
+
+    result = service.create(data, session=session)
+
+    assert result.id == id
+    assert result.created_at
+
+
+def test_topic_service_get(session, topic):
+    service = TopicService()
+
+    result = service.get(topic.id, session=session)
+
+    assert result.id == topic.id
+    assert result.created_at
+
+
+def test_topic_service_get_not_found(session):
+    service = TopicService()
+
+    with pytest.raises(NotFoundError):
+        service.get("invalid-topic-name", session=session)
+
+
+def test_topic_service_delete(session, topic):
+    service = TopicService()
+
+    assert service.delete(topic.id, session=session) is None
+
+    with pytest.raises(NotFoundError):
+        service.get(topic.id, session=session)
+
+
+def test_topic_service_delete_not_found(session):
+    service = TopicService()
+
+    with pytest.raises(NotFoundError):
+        service.delete("invalid-topic-name", session=session)
